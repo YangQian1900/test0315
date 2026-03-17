b000a7be85fc9fe4fed37586a30b319c2fd1f8cf
YangQian1900 <1107402783@qq.com>
2026-03-17 15:59:11 +0800

    单元测试：import-helper函数getVarInfo

--- src/pages/VariableTable/logic/import-helper.spec.ts
+++ src/pages/VariableTable/logic/import-helper.spec.ts
@@ -1,36 +1,36 @@
 import { describe, it, expect } from "vitest";
-import { textToStrArr } from "./import-helper";
-
-//#region 测试数据
-const validText = `
-VAR
-isReady : BOOL := TRUE; // System ready flag
-counter : INT := 0; // Counter
-temperature : INT;
-END_VAR
-`;
-const validTextArr = [
-  "isReady : BOOL := TRUE; // System ready flag",
-  "counter : INT := 0; // Counter",
-  "temperature : INT;",
-];
-const invalidText1 = `
-isReady : BOOL := TRUE; // System ready flag
-counter : INT := 0; // Counter
-temperature : INT;
-`;
-const invalidText2 = `
-VAR
-END_VAR
-`;
-const invalidText3 = `
-VAR
-
-END_VAR
-`;
-//#endregion 测试数据
+import { getVarInfo, textToStrArr } from "./import-helper";
 
 describe("textToStrArr", () => {
+  //#region 测试数据
+  const validText = `
+VAR
+isReady : BOOL := TRUE; // System ready flag
+counter : INT := 0; // Counter
+temperature : INT;
+END_VAR
+`;
+  const validTextArr = [
+    "isReady : BOOL := TRUE; // System ready flag",
+    "counter : INT := 0; // Counter",
+    "temperature : INT;",
+  ];
+  const invalidText1 = `
+isReady : BOOL := TRUE; // System ready flag
+counter : INT := 0; // Counter
+temperature : INT;
+`;
+  const invalidText2 = `
+VAR
+END_VAR
+`;
+  const invalidText3 = `
+VAR
+
+END_VAR
+`;
+  //#endregion 测试数据
+
   it("should parse valid text", () => {
     const result = textToStrArr(validText);
     expect(result).toEqual(validTextArr);
@@ -41,5 +41,37 @@ describe("textToStrArr", () => {
     expect(() => textToStrArr(invalidText1)).toThrow();
     expect(() => textToStrArr(invalidText2)).toThrow();
     expect(() => textToStrArr(invalidText3)).toThrow();
+  });
+});
+
+describe("getVarInfo", () => {
+  //#region 测试数据
+  const validStr1 = "isReady : BOOL := TRUE; // System ready flag";
+  const validStr2 = "temperature : INT;";
+  const invalidStr1 = "isReady Bool false";
+  const validStr1Result = {
+    index: -1,
+    name: "isReady",
+    dataType: "BOOL",
+    defaultValue: "TRUE",
+    comment: "System ready flag",
+  };
+  const validStr2Result = {
+    index: -1,
+    name: "temperature",
+    dataType: "INT",
+    defaultValue: undefined,
+    comment: undefined,
+  };
+  //#endregion 测试数据
+
+  it("should extract info from valid string", () => {
+    let result = getVarInfo(validStr1);
+    expect(result).toEqual(validStr1Result);
+    result = getVarInfo(validStr2);
+    expect(result).toEqual(validStr2Result);
+  });
+  it("should throw when string is invalid", () => {
+    expect(() => getVarInfo(invalidStr1)).toThrow();
   });
 });
